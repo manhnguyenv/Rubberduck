40b8e02e2112cdd90226543e46c18c5c56283f6b
Mathieu Guindon <retailcoder@gmail.com>
2016-07-18 16:32:57 -0400

    WinForms UI harmonized with other RD dialogs

--- RetailCoder.VBE/UI/ReferenceBrowser/ReferenceBrowserWindow.Designer.cs
+++ RetailCoder.VBE/UI/ReferenceBrowser/ReferenceBrowserWindow.Designer.cs
@@ -28,17 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReferenceBrowserWindow));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.InstructionsLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CancelDialogButton = new System.Windows.Forms.Button();
+            this.OkButton = new System.Windows.Forms.Button();
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.referenceBrowser = new Rubberduck.UI.ReferenceBrowser.ReferenceBrowser();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.TitleLabel);
+            this.panel1.Controls.Add(this.InstructionsLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 64);
+            this.panel1.TabIndex = 2;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Padding = new System.Windows.Forms.Padding(2);
+            this.TitleLabel.Size = new System.Drawing.Size(160, 19);
+            this.TitleLabel.TabIndex = 4;
+            this.TitleLabel.Text = "[ActiveVBProject.Name]";
+            // 
+            // InstructionsLabel
+            // 
+            this.InstructionsLabel.AutoSize = true;
+            this.InstructionsLabel.Location = new System.Drawing.Point(9, 30);
+            this.InstructionsLabel.Name = "InstructionsLabel";
+            this.InstructionsLabel.Padding = new System.Windows.Forms.Padding(4);
+            this.InstructionsLabel.Size = new System.Drawing.Size(195, 21);
+            this.InstructionsLabel.TabIndex = 5;
+            this.InstructionsLabel.Text = "Add/remove references to the project.";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.flowLayoutPanel2.Controls.Add(this.CancelDialogButton);
+            this.flowLayoutPanel2.Controls.Add(this.OkButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 318);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(8, 8, 0, 8);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(484, 43);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // CancelDialogButton
+            // 
+            this.CancelDialogButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.CancelDialogButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelDialogButton.Location = new System.Drawing.Point(398, 11);
+            this.CancelDialogButton.Name = "CancelDialogButton";
+            this.CancelDialogButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelDialogButton.TabIndex = 0;
+            this.CancelDialogButton.Text = "Cancel";
+            this.CancelDialogButton.UseVisualStyleBackColor = false;
+            // 
+            // OkButton
+            // 
+            this.OkButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OkButton.Location = new System.Drawing.Point(317, 11);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = false;
             // 
             // elementHost1
             // 
             this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Location = new System.Drawing.Point(0, 64);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(484, 361);
-            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Size = new System.Drawing.Size(484, 254);
+            this.elementHost1.TabIndex = 5;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = this.referenceBrowser;
             // 
@@ -48,15 +124,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
             this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReferenceBrowserWindow";
-            this.Text = "Reference Browser";
+            this.Text = "Rubberduck - Project References";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label InstructionsLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button CancelDialogButton;
+        private System.Windows.Forms.Button OkButton;
         private System.Windows.Forms.Integration.ElementHost elementHost1;
         private ReferenceBrowser referenceBrowser;
+
     }
 }
